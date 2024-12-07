6ce76d97db58a05381cb3105bfa501b161354002
Oles <gedzoles@gmail.com>
2024-12-07 23:53:22 +0200

    avian to 0.15

---
+++ projects/shooter/src/main.rs
@@ -0,0 +1,51 @@
+use bevy::{
+    prelude::*
+};
+use avian3d::prelude::*;
+fn main() {
+    App::new()
+        .add_plugins((DefaultPlugins))
+        .add_systems(Startup, setup)
+        .add_systems(Update, (rotate))
+        .run();
+}
+
+/// Set up a simple 3D scene
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    // camera
+    commands.spawn((
+        Camera3d::default(),
+        Transform::from_translation(Vec3::new(0.0, 0.0, 5.0)).looking_at(Vec3::default(), Vec3::Y),
+    ));
+
+    // cube
+    commands.spawn((
+        Mesh3d(meshes.add(Cuboid::default())),
+        MeshMaterial3d(materials.add(Color::srgb(0.8, 0.7, 0.6))),
+        Transform::from_xyz(0.0, 0.5, 0.0),
+        Rotates,
+    ));
+    // light
+    commands.spawn(DirectionalLightBundle {
+        directional_light: DirectionalLight {
+            illuminance: 1_000.,
+            ..default()
+        },
+        ..default()
+    });
+}
+
+#[derive(Component)]
+struct Rotates;
+
+/// Rotates any entity around the x and y axis
+fn rotate(time: Res<Time>, mut query: Query<&mut Transform, With<Rotates>>) {
+    for mut transform in &mut query {
+        transform.rotate_x(0.55 * time.delta_secs());
+        transform.rotate_z(0.15 * time.delta_secs());
+    }
+}
