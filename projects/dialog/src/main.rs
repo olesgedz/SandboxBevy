5aef7b82ba7239e09e7b820eeae78d88e6bb6151
Oles <gedzoles@gmail.com>
2024-12-22 01:49:51 +0200

    clicking

--- projects/dialog/src/main.rs
+++ projects/dialog/src/main.rs
@@ -19,22 +19,11 @@ fn main() {
 
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     let font = asset_server.load("fonts/Montserrat-Bold.ttf");
-    let text_style = TextFont {
-        font: font.clone(),
-        font_size: 50.0,
-        ..default()
-    };
+
     // 2d camera
     commands.spawn(Camera2d::default());
     // Demonstrate changing translation
-
-    // // Demonstrate text wrapping
-    let slightly_smaller_text_style = TextFont {
-        font,
-        font_size: 42.0,
-        ..default()
-    };
-    let box_size = Vec2::new(300.0, 200.0);
+    
     let box_position = Vec2::new(0.0, -250.0);
     commands
         .spawn((
@@ -47,9 +36,6 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     
     commands.spawn((
         Sprite::from_image(asset_server.load("characters/alice/Alice_Default.png")),
-        // texture: asset_server.load("characters/alice/Alice_Default.png"),
-        // transform: Transform::from_translation(Vec3::new(500.0, -10.0, 1.0))
-        //     .with_scale(Vec3::splat(0.25)),
         Transform::from_translation(Vec3::new(500.0, -10.0, 1.0))
             .with_scale(Vec3::splat(0.25)),
         ))
@@ -66,51 +52,33 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
             let mut text = texts.get_single_mut().unwrap();
             text.0 = "Up".parse().unwrap();
             println!("Up {:?}", out);
-        });;;;
-    // commands
-    //     .spawn(NodeBundle {
-    //         style: Style {
-    //             width: Val::Percent(100.),
-    //             height: Val::Percent(100.0),
-    //             align_items: AlignItems::Center,
-    //             justify_content: JustifyContent::Center,
-    //             flex_direction: FlexDirection::Column,
-    //             ..default()
-    //         },
-    //         ..default()
-    //     })
-    //     .with_children(|parent| {
-    //         parent
-    //             .spawn(NodeBundle {
-    //                 style: Style {
-    //                     width: Val::Px(250.),
-    //                     height: Val::Px(250.),
-    //                     margin: UiRect::bottom(Val::Px(15.)),
-    //                     ..default()
-    //                 },
-    //                 background_color: Color::srgb(235., 35., 12.).into(),
-    //                 ..default()
-    //             })
-    //             .insert(RelativeCursorPosition::default());
-    // 
-    //         parent.spawn(TextBundle {
-    //             text: Text::from_section(
-    //                 "(0.0, 0.0)",
-    //                 TextStyle {
-    //                     font: asset_server.load("fonts/Montserrat-Bold.ttf"),
-    //                     font_size: 40.0,
-    //                     color: Color::srgb(0.9, 0.9, 0.9),
-    //                 },
-    //             ),
-    //             ..default()
-    //         });
-    //     });
+        });
+
+
+    let other_box_size = Vec2::new(300.0, 100.0);
+    let other_box_position = Vec2::new(50.0, -150.0);
+    let slightly_smaller_text_font = TextFont {
+        font,
+        font_size: 15.0,
+        ..default()
+    };
+    commands
+        .spawn((
+            Sprite::from_color(Color::srgb(0.20, 0.3, 0.70), other_box_size),
+            Transform::from_translation(other_box_position.extend(0.0)),
+        ))
+        .with_children(|builder| {
+            builder.spawn((
+                Text2d::new("this text wraps in the box\n(AnyCharacter linebreaks)"),
+                slightly_smaller_text_font.clone(),
+                TextLayout::new(JustifyText::Left, LineBreak::AnyCharacter),
+                // Wrap text in the rectangle
+                TextBounds::from(other_box_size),
+                // ensure the text is drawn on top of the box
+                Transform::from_translation(Vec3::Z),
+            ));
+        });
 }
-// .observe(|out: Trigger<Pointer<Down>>, mut texts: Query<&mut Text2d>| {
-//     let mut text = texts.get_mut(out.entity()).unwrap();
-//     text.0 = "Down".parse().unwrap();
-//     println!("Down {:?}", out);
-// });;
 
 fn recolor_on<E: Debug + Clone + Reflect>(color: Color) -> impl Fn(Trigger<E>, Query<&mut Sprite>) {
     move |ev, mut sprites| {
