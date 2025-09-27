cdff1d68e50fcb77db6974fb5aef402b3cff3857
o.gedz <o.gedz@sportradar.com>
2025-09-27 09:26:24 +0300

    add turn_test project with initial setup and basic camera system

---
+++ projects/turn_test/src/main.rs
@@ -0,0 +1,11 @@
+use bevy::prelude::*;
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup)
+        .run();
+}
+fn setup(mut commands: Commands) {
+    commands.spawn(Camera2d);
+}
